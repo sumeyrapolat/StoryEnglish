0fd5bc1a9be6d113807a84acd530a5eb734b61c6
Sümeyra Polat <sumeyrapolaat@gmail.com>
2024-02-14 01:36:26 +0300

    ProfileAdapter are created and wrote

---
+++ app/src/main/java/com/sumeyra/storyenglish/adapter/ProfileAdapter.kt
@@ -0,0 +1,31 @@
+package com.sumeyra.storyenglish.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sumeyra.storyenglish.databinding.ProfileRowBinding
+import com.sumeyra.storyenglish.model.Post
+import com.sumeyra.storyenglish.model.Profile
+
+class ProfileAdapter(val profilePost: ArrayList<Post>) : RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
+    class ProfileViewHolder( val binding: ProfileRowBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
+        val binding = ProfileRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ProfileViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return profilePost.size
+    }
+
+    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
+        holder.binding.profileUsername.text = profilePost.get(position).userName
+        holder.binding.profileStoryHeader.text = profilePost.get(position).userName
+        holder.binding.profileStoryText.text = profilePost.get(position).story
+        holder.binding.profileWordText.text = profilePost.get(position).words
+    }
+}
+
